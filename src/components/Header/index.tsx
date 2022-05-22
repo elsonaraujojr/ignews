c6c56eb73b3c69861c1537107a6a8c4bc07dea01
Elson Júnior <elson.araujo.bw@gmail.com>
2022-05-22 10:03:53 -0400

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import LogoSVG from "../../../public/images/logo.svg";
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src={LogoSVG} alt="Logo" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,59 @@
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
+import AvatarSVG from "./../../public/images/avatar.svg";
+import styles from "./home.module.scss";
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            New about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access ti all the publications <br />
+            <span>for { product.amount} month </span>
+          </p>
+          <SubscribeButton priceId={ product.priceId } />
+        </section>
+        <Image src={AvatarSVG} alt="Avatar" />
+      </main>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1L1MS4FfrvpyaezmDcuN434n", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
