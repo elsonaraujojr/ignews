4560182503fa05f2cc2254bcff536fbe374b5570
Elson Júnior <elson.araujo.bw@gmail.com>
2022-05-22 10:31:14 -0400

    SSR for SSG page static in next.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, GetStaticProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -14,6 +14,11 @@ interface HomeProps {
 }
 
 export default function Home({ product }: HomeProps) {
+
+  // Client side
+  // Server side
+  // Static Side Generation
+
   return (
     <>
       <Head>
@@ -38,7 +43,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1L1MS4FfrvpyaezmDcuN434n", {
     expand: ["product"],
   });
@@ -49,11 +54,12 @@ export const getServerSideProps: GetServerSideProps = async () => {
       style: "currency",
       currency: "BRL",
     }).format(price.unit_amount / 100),
-  }
+  };
 
   return {
     props: {
       product,
     },
+    revalidate: 60 * 60 *24, // 1 day
   };
 };
