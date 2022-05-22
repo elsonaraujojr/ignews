7dae73d983fea1b151a02b5649ee0399fc025c1a
Elson Júnior <elson.araujo.bw@gmail.com>
2022-05-22 11:13:57 -0400

    Exemple users api nextjs

---
+++ src/pages/api/users/[id].ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const users = (request: NextApiRequest, response: NextApiResponse) => {
+
+  console.log(request.query);
+  
+  const user = [
+    { id: 1, name: "Elson Júnior" },
+    { id: 2, name: "Jó Marques" },
+    { id: 3, name: "Maria Coco" },
+  ];
+
+  return response.status(200).json(user);
+};
+
+export default users;
---
+++ src/pages/api/users/index.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const users =  (request: NextApiRequest, response: NextApiResponse) => {
+  const user = [
+    { id: 1, name: "Elson Júnior" },
+    { id: 2, name: "Jó Marques" },
+    { id: 3, name: "Maria Coco" },
+  ];
+
+  return response.status(200).json(user);
+};
+
+export default users;
